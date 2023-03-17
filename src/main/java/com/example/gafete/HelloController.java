1edf9a9da04ba1eabf88292e06850bbaa41767aa
ktalamantes <ch190116879@chapala.tecmm.edu.mx>
2023-03-17 17:24:40 -0600

    imagenes

--- src/main/java/com/example/gafete/HelloController.java
+++ src/main/java/com/example/gafete/HelloController.java
@@ -2,13 +2,16 @@ package com.example.gafete;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ImageView agregarImagen;
+    @FXML
+    private ImageView editarImagen;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
