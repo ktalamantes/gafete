d3c173c583defb164afb57c4aa465ef7b696d584
ktalamantes <ch190116879@chapala.tecmm.edu.mx>
2023-04-02 03:44:46 -0600

    Contraseña

--- src/main/java/com/example/gafete/HelloController.java
+++ src/main/java/com/example/gafete/HelloController.java
@@ -1,10 +1,10 @@
 package com.example.gafete;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -18,11 +18,53 @@ public class HelloController {
     @FXML private ImageView gamoss;
     @FXML
     private Button btnSalirA;
-
     @FXML
-    protected void onHelloButtonClick() {
+    private Button btnSalirE;
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private TextField usuario;
+    @FXML
+    private PasswordField contrasenia;
+    @FXML
+    private CheckBox box;
+    @FXML
+    private ImageView ojo;
 
-    }
+
+    //--------------------Validar contraseña e ingresar----------------------------------
+    @FXML
+    protected void btnValidar(){
+        if (usuario.getText().equals("Administrador") && contrasenia.getText().equals("12345")){
+            try {
+                Stage stage = new Stage();//Crear una nueva ventana
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("gafete.fxml"));
+                Scene escena = new Scene(loader.load());
+                stage.setTitle("editar");
+                stage.setScene(escena);
+                stage.showAndWait();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }//catch
+        }else {
+            System.out.println("ACCESO DENEGADO");
+        }
+    }//----------------Boton validar-------------------------------
+
+    //-------------Muestra la contraseña-------------
+    @FXML
+    void changeVisiility(ActionEvent event){
+        if(box.isSelected()){
+            txtUsuario.setText(contrasenia.getText());
+            txtUsuario.setVisible(true);
+            contrasenia.setVisible(false);
+            return;
+        }
+        contrasenia.setText(txtUsuario.getText());
+        contrasenia.setVisible(true);
+        txtUsuario.setVisible(false);
+    }//Ver contraseña
+
 
     //------------METODO QUE ABRE "CUENTA" DANDO CLICK EN IMAGEN-------------------
     @FXML
@@ -68,13 +110,34 @@ public class HelloController {
             stage.showAndWait();
         } catch (Exception e) {
             e.printStackTrace();
-
         }//catch
     }//boton agregar
 
+    //-------------BOTON QUE PERMITE EDITAR USUARIO/S
+    @FXML
+    protected void btnEditar(){
+        try {
+            Stage stage = new Stage();//Crear una nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("editar.fxml"));
+            Scene escena = new Scene(loader.load());
+            stage.setTitle("editar");
+            stage.setScene(escena);
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }//catch
+    }//Boton editar
+
+    //-----------------Boton salir en ventana de agregar-------------------
     @FXML
     protected void btnSalirAgregar(){
         Stage s = (Stage) btnSalirA.getScene().getWindow();
+        s.close();
+    }//boton salirAgregar
+
+    @FXML
+    protected void btnSalirEditar(){
+        Stage s = (Stage) btnSalirE.getScene().getWindow();
         s.close();
     }
 }
