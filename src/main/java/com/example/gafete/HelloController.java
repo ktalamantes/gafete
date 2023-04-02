8cb45b351ebf2ed0535a0a67bd77436119232cc5
ktalamantes <ch190116879@chapala.tecmm.edu.mx>
2023-04-02 02:11:36 -0600

    avance

--- src/main/java/com/example/gafete/HelloController.java
+++ src/main/java/com/example/gafete/HelloController.java
@@ -1,8 +1,12 @@
 package com.example.gafete;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
@@ -12,9 +16,65 @@ public class HelloController {
     @FXML private ImageView tec;
     @FXML private ImageView candado1;
     @FXML private ImageView gamoss;
+    @FXML
+    private Button btnSalirA;
 
     @FXML
     protected void onHelloButtonClick() {
 
+    }
+
+    //------------METODO QUE ABRE "CUENTA" DANDO CLICK EN IMAGEN-------------------
+    @FXML
+    protected void imagenAgregar(){
+        try {
+            Stage stage = new Stage();//Crear una nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Scene escena = new Scene(loader.load());
+            stage.setTitle("editar");
+            stage.setScene(escena);
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }//catch
+    }//Imagen Agregar
+
+    //------------METODO QUE ABRE "AGREGAR" DANDO CLICK EN IMAGEN-------------------
+    @FXML
+    protected void imagenCuenta(){
+        try {
+            Stage stage = new Stage();//Crear una nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Agregar.fxml"));
+            Scene escena = new Scene(loader.load());
+            stage.setTitle("cuenta");
+            stage.setScene(escena);
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }//catch
+    }//Imagen cuenta
+
+    //-----------BOTON QUE AGREGA USUARIO------------
+    @FXML
+    protected void btnAgregar(){
+        try {
+            Stage stage = new Stage();//Crear una nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AgregarUsuario.fxml"));
+            Scene escena = new Scene(loader.load());
+            stage.setTitle("editar");
+            stage.setScene(escena);
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }//catch
+    }//boton agregar
+
+    @FXML
+    protected void btnSalirAgregar(){
+        Stage s = (Stage) btnSalirA.getScene().getWindow();
+        s.close();
     }
 }
